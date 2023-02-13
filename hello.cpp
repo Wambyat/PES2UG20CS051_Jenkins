1d45d3732ea6001a3fd9ebe3417a3aa9b6b6a4f1
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-02-13 09:22:54 +0530

    lab work

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "PES2UG20CS051\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
