81292a530b85cafdc8c800c8e223565ae61e49ce
Anirudha Anekal <77496170+Wambyat@users.noreply.github.com>
2023-02-15 11:18:23 +0530

    ano

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(void) {
   cout << "PES2UG20CS051\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
+  cout << "Edit\n";
   return 0;
 }
